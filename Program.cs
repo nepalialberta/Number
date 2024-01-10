85e856b7fa78b8287058fbe479478d02670f7971
albertanepali <albertanepali@gmail.com>
2024-01-09 19:45:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//Reverse the number 
+
+class Program
+{
+
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the number here");
+
+        int Number = int.Parse(Console.ReadLine());
+        int Reverse = 0;
+        while (Number > 0)
+        {
+            int remainder = Number % 10;
+            Reverse = (Reverse * 10) + remainder;
+            Number = Number / 10;
+
+
+        }
+
+        Console.WriteLine("Your reverse number is  "+Reverse);
+        Console.ReadLine();
+    }
+}
